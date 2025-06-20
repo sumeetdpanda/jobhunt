288218a6f41caab127f4ae6881d20324b61d63ea
Sumeet Shrivastava <sumeetshrivas@yahoo.com>
2025-06-20 21:34:23 +0530

    DB Connection

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,10 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
 
+dotenv.config({});
 const app = express();
 
 // Middleware
@@ -15,8 +18,9 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
+  connectDB();
   console.log(`Server running at port ${PORT}`);
 });
